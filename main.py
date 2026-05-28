8b07a5589bd86c00910e240be2f6476b18fbfadc
lazy developer <iamsiva6762@gmail.com>
2026-05-28 21:38:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,10 +15,12 @@ else:
     users = []
 
 os.system("cls")
+print("\033[92m+-----------------------------------------------+")
+print("|                  \033[1;95mREEL-MAKER\033[92m                   |")
 print("+-----------------------------------------------+")
-print("|                  REEL-MAKER                   |")
+print("|                                               |")
 print("+-----------------------------------------------+")
-f_msg = input("\033[33mENTER THE TOPIC : ")
+f_msg = input("\033[4;2H\033[33mENTER THE TOPIC : ")
 
 folder = re.sub(r'[^A-Za-z0-9]','_',f_msg)
 base_path = f"output/{folder}"
@@ -26,8 +28,8 @@ os.makedirs(base_path,exist_ok=True)
 audio_path = (f"{base_path}/{folder}.wav")
 video_path = (f"{base_path}/{folder}.mp4")
 os.system("cls")
-print("+-----------------------------------------------+")
-print(f"|             REEL-MAKER ({f_msg})              ")
+print("\033[92m+-----------------------------------------------+")
+print(f"|             \033[1;95mREEL-MAKER\033[94m({f_msg})\033[92m               ")
 print("\033[2;49H|")
 print("+-----------------------------------------------+")
 print("|                                               |")
@@ -42,13 +44,13 @@ script = msg_out["script"]
 caption = msg_out["caption"]
 
 script = re.sub(r'[^A-Za-z0-9\u0C00-\u0C7F\s]','',script)
-print("\033[4;2H[+] VOICE GENERATING...")
+print("\033[94m\033[4;2H[+] VOICE GENERATING...")
 v_engine(
     script,
     audio_path
 )
-print("\033[5;2H[+] VOICE GENERATION COMPLETE...")
-print("\033[6;2H[+] VIDEO GENRATION STRATED...")
+print("\033[1;94m\033[5;2H[+] VOICE GENERATION COMPLETE...")
+print("\033[1;94m\033[6;2H[+] VIDEO GENRATION STRATED...")
 
 v2_engine(
     script=script,
@@ -57,8 +59,8 @@ v2_engine(
     background_folder="backgrounds",
     voice_file=audio_path
 )
-print("\033[7;2H[+] VIDEO GENERATION COMPLETE...")
+print("\033[1;94m\033[7;2H[+] VIDEO GENERATION COMPLETE...")
 data = users[0]
-print(f"\033[8;2H[+] UPLOADING ON INSTAGRAM [{data['user']}]")
+print(f"\033[1;94m\033[8;2H[+] UPLOADING ON INSTAGRAM [{data['user']}]")
 snd(data["user"],data["pass"],caption,video_path)
-print(f"\033[9;2H[+]VIDEO UPLOADED AT [{data['user']}]")
+print(f"\033[1;94m\033[9;2H[+]VIDEO UPLOADED AT [{data['user']}]")
